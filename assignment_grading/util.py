bcb72f77c60c58f8c40e74a38f3a0ece2b5161c8
Tanawat Khunlertkit <tkhunlertkit@gmail.com>
2018-09-26 17:26:20 -0500

    assignment grading script to extract each student

---
+++ assignment_grading/util.py
@@ -0,0 +1,31 @@
+import os
+from zipfile import ZipFile
+
+ASSIGNMENT_DIR = './assignment/'
+EXTRACT_DIR = './extracted/'
+
+def extract_student_name(filename: str):
+    try:
+        first_index = filename.index('-', filename.index('-') + 1)
+        last_index = filename.index('-', first_index + 1)
+        name = filename[first_index + 1:last_index].strip()
+        return name
+    except:
+        return filename
+
+def unzip(filename, destination):
+    with ZipFile(ASSIGNMENT_DIR + filename, 'r') as zf:
+        zf.extractall(EXTRACT_DIR + destination)
+
+if __name__ == '__main__':
+    zip_list = sorted([x for x in os.listdir('./assignment') if x.endswith('.zip')])
+    zip_list.append('')
+    num_student = 0
+    for file_a, file_b in zip(zip_list, zip_list[1:]):
+        name_a = extract_student_name(file_a)
+        name_b = extract_student_name(file_b)
+        if name_a != name_b:
+            unzip(file_a, name_a)
+            num_student += 1
+
+    print('total studnet : %d' % num_student)
