0947cc643f00eeb9d3ab90aa36e47f867d503a50
Tanawat Khunlertkit <tkhunlertkit@gmail.com>
2018-09-28 23:49:46 -0500

    more interesting lab 5 version

--- Lab 5/js/accordian.js
+++ Lab 5/js/accordian.js
@@ -1,7 +1,29 @@
+var swapElements = {};
+
 var createAccordian = function(accordianElem) {
 	// TODO: Implement createAccordian.
+	let index = $(".accordian").index(accordianElem.prev());
+	if (swapElements[index].count % 2 == 0) {
+		let temp = swapElements[index].text;
+		swapElements[index].text = accordianElem.html();
+		accordianElem.html(temp);
+	}
+
+	accordianElem.slideToggle(200);
+	swapElements[index].count++;
 };
 
 $(document).ready(function() {
 	// TODO: Use createAccordian to made the FAQ page expandable and collapsible.
+	$(".accordian").each(function(i, obj) {
+		$(this).next().hide();
+		swapElements[i] = {
+			count: 0,
+			text: "click me more to reveal the secret",
+		};
+	});
+
+	$(".accordian").click(function() {
+		createAccordian($(this).next());
+	});
 });
